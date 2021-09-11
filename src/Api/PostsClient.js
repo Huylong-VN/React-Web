2fb96dca5a4d4fd7697068db94f3b520f88f72d6
Huylong-VN <adultsforfuture@gmail.com>
2021-09-11 16:18:06 +0700

    a

--- src/Api/PostsClient.js
+++ src/Api/PostsClient.js
@@ -7,5 +7,29 @@ const postApi=({
             Authorization:"Bearer "+localStorage.getItem("token")
         }})
     },
+    createAsync:async(params)=>{
+        const url='/posts/';
+        return await axiosClient.post(url,{params,headers:{
+            Authorization:"Bearer "+localStorage.getItem("token")
+        }})
+    },
+    updateAsync:async(params)=>{
+        const url='/posts/';
+        return await axiosClient.put(url,{params,headers:{
+            Authorization:"Bearer "+localStorage.getItem("token")
+        }})
+    },
+    deleteAsync:async(params)=>{
+        const url='/posts/';
+        return await axiosClient.delete(url,{params,headers:{
+            Authorization:"Bearer "+localStorage.getItem("token")
+        }})
+    },
+    grantPermissionAsync:async(params)=>{
+        const url='/posts/addpresmission/';
+        return await axiosClient.post(url,{params,headers:{
+            Authorization:"Bearer "+localStorage.getItem("token")
+        }})
+    }
 })
 export default postApi
--- src/Layout/Client.js
+++ src/Layout/Client.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Layout, Row, Col, Button } from 'antd'
 import "./Client.css"
+import { useMediaQuery } from 'react-responsive'
 
 export const Client = (props) => {
     const { Header, Footer, Content } = Layout;
-    const styles=({ 
+    const isTabletOrMobile = useMediaQuery({ maxWidth: 556 })
+    const styles = ({
         width: {
             width: '70%',
         },
@@ -32,43 +34,53 @@ export const Client = (props) => {
             height: "100vh"
         }
     })
-    
+
     return (
         <Layout style={styles.container}>
             <Header className="header">
                 <div className="appMenu">
                     <Row>
                         {/* Menu tren */}
-                        <Col style={styles.appName} span={8}>News COVID</Col>
-           
+                        
+                        {isTabletOrMobile ? (
+                            <>
+                            <Col style={styles.appName} span={23}>News COVID</Col>
+                            <Col style={styles.icon} span={1}><i className="fas fa-list"></i></Col>
+                            </>
+                        ) : (
+                            <>
+                            <Col style={styles.appName} span={10}>News COVID</Col>
                         <Col style={styles.inputSearch} span={7}></Col>
-                        <Col style={styles.icon} span={2}><i className="fas fa-list"></i></Col>
-                        <Col style={styles.icon} span={2}><i className="fab fa-artstation"></i></Col>
-                        <Col style={styles.icon} span={2}><i className="fas fa-bell"></i></Col>
-             
-                        <Col style={styles.icon} span={2}><i className="fas fa-cogs"></i></Col>
-                        <Col style={styles.icon} span={1}><Button style={styles.login}>Login</Button></Col>
+                                <Col style={styles.icon} span={2}><i className="fab fa-artstation"></i></Col>
+                                <Col style={styles.icon} span={2}><i className="fas fa-bell"></i></Col>
+
+                                <Col style={styles.icon} span={2}><i className="fas fa-cogs"></i></Col>
+                                <Col style={styles.icon} span={1}><Button style={styles.login}>Login</Button></Col>
+                            </>
+                        )}
+                    
+
                         {/* End Menu tren */}
-                       
+
                     </Row>
                 </div>
             </Header>
-            <Content style={{ backgroundColor: 'white',}}>{props.children}</Content>
-            <Footer style={{alignItems: 'center'}}>
+            <Content style={{ backgroundColor: 'white', }}>{props.children}</Content>
+            <Footer style={{ alignItems: 'center' }}>
                 <Row>
                     <Col span={8}>
-                       <p>ok</p>
+                        <p>ok</p>
                     </Col>
                     <Col span={8}>
-                            <div><h4>Thông tin liên hệ</h4></div>
-                            <div><p>Địa chỉ: 107 Nguyễn Phong Sắc, Cầu giấy, Hà Nội</p></div>
-                            <div><p>Số điện thoại: 0245874168554</p></div>
-                            <div><p>Email: T4H@gmail.com</p></div>
+                        <div><h4>Thông tin liên hệ</h4></div>
+                        <div><p>Địa chỉ: 107 Nguyễn Phong Sắc, Cầu giấy, Hà Nội</p></div>
+                        <div><p>Số điện thoại: 0245874168554</p></div>
+                        <div><p>Email: T4H@gmail.com</p></div>
                     </Col>
                     <Col span={8}>
-                            <div><h4>Một số liên kết Một số liên kết</h4></div>
-                            <div>Yêu cầu hỗ trợ Một số liên kết a a a a </div>
-                            <div>Báo cáo lỗi Một số liên kết a a a a </div>
+                        <div><h4>Một số liên kết Một số liên kết</h4></div>
+                        <div>Yêu cầu hỗ trợ Một số liên kết a a a a </div>
+                        <div>Báo cáo lỗi Một số liên kết a a a a </div>
                     </Col>
                 </Row>
             </Footer>
